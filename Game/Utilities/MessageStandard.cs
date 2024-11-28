889a7353cdc5d19395e36a85c72a8ca13d831200
Jukka Keinänen <jukka.keinane@gmail.com>
2024-11-28 08:22:52 +0200

    Add standard message format for validation responses

---
+++ Game/Utilities/MessageStandard.cs
@@ -0,0 +1,8 @@
+namespace MUS.Game.Utilities;
+
+static public class MessageStandard
+{
+    static public string Invalid(string input) => $"{input} is not valid.";
+
+    static public string NotExists(string input) => $"{input} does not exist.";
+}
