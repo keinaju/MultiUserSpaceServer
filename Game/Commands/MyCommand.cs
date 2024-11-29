87bb600a740131cb776708536e4a687c9b82b8d3
Jukka Keinänen <jukka.keinane@gmail.com>
2024-11-29 13:58:17 +0200

    Add dot

--- Game/Commands/MyCommand.cs
+++ Game/Commands/MyCommand.cs
@@ -31,6 +31,6 @@ public class MyCommand : BaseCommand
             );
         }
 
-        return $"{being.Name} has: {inventory.Contents()}";
+        return $"{being.Name} has: {inventory.Contents()}.";
     }
 }
