357a010771a04822317f5c4f494a71db498932d7
Jukka Keinänen <jukka.keinane@gmail.com>
2024-12-03 12:43:02 +0200

    Add cloning in being

--- Game/Data/Models/Being.cs
+++ Game/Data/Models/Being.cs
@@ -8,19 +8,32 @@ public class Being
     public int PrimaryKey { get; set; }
 
     public int CreatedByUserPrimaryKey { get; set; }
-    public User CreatedByUser { get; set; } = null!;
-
-    public required Inventory Inventory { get; set; }
-
-    public required string? Name { get; set; }
+    public required User CreatedByUser { get; set; } = null!;
 
     /// <summary>
     /// Location of being.
     /// </summary>
     public required Room InRoom { get; set; }
 
+    public required Inventory Inventory { get; set; }
+
+    public required string? Name { get; set; }
+
     /// <summary>
     /// Optional room inside being, e.g. vehicle room.
     /// </summary>
     public Room? RoomInside { get; set; }
+
+    public Being Clone()
+    {
+        return new Being()
+        {
+            CreatedByUser = this.CreatedByUser,
+            InRoom = this.InRoom,
+            Inventory = this.Inventory.Clone(),
+            Name = this.Name,
+            RoomInside = this.RoomInside is null ?
+                null : this.RoomInside.Clone()
+        };
+    }
 }
